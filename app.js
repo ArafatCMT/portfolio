712200af66fc404ebf98ba4574d6a216b0acef82
ArafatCMT <arafatemon6969@gmail.com>
2024-11-03 10:22:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+// <!-- project section  -->
+//        <div class="project-container col-lg-10 col-md-12 col-sm-12 mx-auto " style="margin-top: 100px;" id="projects">
+//         <h2 class="text-center">MY RECENT BEST WORKS</h2>
+
+
+//         <div class="col-lg-12 col-md-12 col-sm-12 mt-5 mb-5">
+//           <div class="card mb-3">
+//             <div class="row g-0">
+//               <div class="col-md-5">
+//                 <!-- <img src="./images/p1.png" class="img-fluid rounded-start" alt="..."> -->
+//                 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+//                   <div class="carousel-inner">
+//                     <div class="carousel-item active" data-bs-interval="10000">
+//                       <img src="./images/p1.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item" data-bs-interval="2000">
+//                       <img src="./images/p1a.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item">
+//                       <img src="./images/p1b.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item" data-bs-interval="2000">
+//                       <img src="./images/p1c.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item" data-bs-interval="2000">
+//                       <img src="./images/p1d.png" class="d-block w-100" alt="...">
+//                     </div>
+//                   </div>
+//                   <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+//                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+//                     <span class="visually-hidden">Previous</span>
+//                   </button>
+//                   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+//                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+//                     <span class="visually-hidden">Next</span>
+//                   </button> -->
+//                 </div>
+//               </div>
+//               <div class="col-md-7">
+//                 <div class="card-body">
+//                   <h3 class="card-title">Net Book</h3>
+//                   <p class="card-text ">Netbook is a social media platform where users can create customizable profiles with profile pictures, bios, and personal details. Users can connect as friends, view each other’s activities, and share posts with text and images. It includes features like comments, likes, dislikes, and options to edit or delete posts and comments, giving users control over their interactions.</p>
+//                   <p class="card-text"><b>Technoloy</b></p>
+//                   <ul>
+//                     <li>Django</li>
+//                     <li>Django REST API</li>
+//                     <li>JavaScript</li>
+//                     <li>CSS, Bootstrap</li>
+//                     <li>HTML</li>
+//                   </ul>
+//                   <div class="d-flex justify-content-between mt-4">
+//                     <p><a href="https://github.com/ArafatCMT/net-book-frontend-2" class="btn btn-primary">Github Ripository</a></p>
+//                     <p><a href="https://arafatcmt.github.io/net-book-frontend-2/login.html" class="btn btn-primary">Live Site</a></p>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div class="col-lg-12 col-md-12 col-sm-12 ">
+//           <div class="card mb-3">
+//             <div class="row g-0">
+//               <div class="col-md-5">
+//                 <!-- <img src="./images/p2.png" class="img-fluid rounded-start" alt="..."> -->
+//                 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+//                   <div class="carousel-inner">
+//                     <div class="carousel-item active" data-bs-interval="10000">
+//                       <img src="./images/p2.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item" data-bs-interval="2000">
+//                       <img src="./images/p2-a.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item">
+//                       <img src="./images/p2-d.png" class="d-block w-100" alt="...">
+//                     </div>
+//                     <div class="carousel-item" data-bs-interval="2000">
+//                       <img src="./images/p2-b.png" class="d-block w-100" alt="...">
+//                     </div>
+//                   </div>
+//                   <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+//                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+//                     <span class="visually-hidden">Previous</span>
+//                   </button>
+//                   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+//                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+//                     <span class="visually-hidden">Next</span>
+//                   </button> -->
+//                 </div>
+//               </div>
+//               <div class="col-md-7">
+//                 <div class="card-body">
+//                   <h3 class="card-title">Smart Soft</h3>
+//                   <p class="card-text">SmartSoft is a comprehensive point-of-sale software application that manages users, products, sales, purchases, stock, and other entities essential to store management.</p>
+//                   <p class="card-text"><b>Technology</b></p>
+//                   <ul>
+//                     <li>Django</li>
+//                     <li>Django REST API</li>
+//                     <li>JavaScript</li>
+//                     <li>CSS, Bootstrap</li>
+//                     <li>HTML</li>
+//                   </ul>
+//                   <div class="d-flex justify-content-between mt-4">
+//                     <p><a href="https://github.com/ArafatCMT/smart_soft-frontend-codes" class="btn btn-primary">Github Ripository</a></p>
+//                     <p><a href="https://arafatcmt.github.io/smart_soft-frontend-codes/index.html" class="btn btn-primary">Live Site</a></p>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         </div>
+
+//         </div>
+
+//        </div>
+//       <!-- project section end -->
